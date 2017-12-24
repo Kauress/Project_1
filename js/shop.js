40b3ec7263c558f22d68f91a88bb3fe9f46a66c3
Kauress <kauress8@gmail.com>
2017-12-24 22:52:35 +0530

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -394,3 +394,18 @@ function removeItem(index) {
     productTotal();
     buildCart();
 }// end of function
+
+
+//function coupon
+function coupon() {
+    var couponInput = document.getElementById("couponCode");
+    if(couponInput.value === "30%Off") {
+        cart.couponValue = 0.30;
+    } else if(couponInput.value === "50%Off") {
+       cart.couponValue = 0.50;
+    } else {
+      cart.couponValue = 1;
+    }
+    productTotal();
+}// end of function
+
