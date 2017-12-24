6f8b6e53210af5275db48b33df9f630abc08c738
Kauress <kauress8@gmail.com>
2017-12-24 22:51:07 +0530

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -364,3 +364,17 @@ function decreaseQuantity(index) {
     }// end of for
     productTotal();
 }// end of function
+
+//increase quantity
+function increaseQuantity(index) {
+    for(var i = 0; i < cart.items.length; i++) {
+      if(cart.items[i].Id === index) {
+        cart.items[i].Quantity++;
+        cart.items[i].Amount = cart.items[i].Quantity * cart.items[i].Price;
+        if(cart.items[i].Quantity >= 101) {
+          removeItem(index);
+        }// end of if
+      }// end of if
+    }// end of for
+    productTotal();
+}// end of function
