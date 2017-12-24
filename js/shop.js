145837a416e010f557fcb47c7c529fb2af486a75
Kauress <kauress8@gmail.com>
2017-12-24 22:47:52 +0530

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -194,3 +194,15 @@ function updateFilter() {
       }// end of if
     }// end of for
 }//end of function
+
+// Cart object
+var cart = {
+items: [],
+numberItems: 0,
+total: 0,
+coupon: ["50%Off", "30%Off"],
+wishlist: [],
+couponValue: 1,
+shipping: 0,
+shippingLocation: ""
+};
