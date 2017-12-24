283c4f5ca764f135fb83e3c3411be04654e0ff72
Kauress <kauress8@gmail.com>
2017-12-24 22:51:56 +0530

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -378,3 +378,19 @@ function increaseQuantity(index) {
     }// end of for
     productTotal();
 }// end of function
+
+// function removeItem
+function removeItem(index) {
+    for(var i = 0; i < cart.items.length; i++) {
+      if(cart.items[i].Id === index) {
+        document.getElementById("productsTable").innerHTML = "";
+        cart.items.splice(i, 1);
+      }// end of if
+    }// end of for
+    if(cart.items.length == 0) {
+      document.getElementById("topCartDisplay").value = 0;
+      localStorage.clear();
+    }//end of if
+    productTotal();
+    buildCart();
+}// end of function
