5f74d51e9b2fe0f332d884deb8b85de8cdcc91f3
Kauress <kauress8@gmail.com>
2017-12-24 22:48:47 +0530

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -206,3 +206,32 @@ couponValue: 1,
 shipping: 0,
 shippingLocation: ""
 };
+
+
+// function getProductInfo
+function getProductInfo(index) {
+  var quantity = parseInt(document.getElementById("product-qty" + index).value);
+  //alert(quantity);
+  var product = {
+    Id: index,
+    Name: products[index].Name,
+    Description: products[index].Description,
+    Price: products[index].Price,
+    Amount: products[index].Price * quantity,
+    Quantity: quantity
+  }; // end of object
+  //alert(product.Id);
+  var productAlreadyInCart = false;
+  for(var i = 0; i < cart.items.length; i++) {
+    if(cart.items[i].Id === index) {
+      productAlreadyInCart = true;
+      cart.items[i].Amount = (product.Amount + cart.items[i].Amount);
+      cart.items[i].Quantity = product.Quantity + cart.items[i].Quantity;
+      break;
+    }// end of if
+  }// end of for
+  if(!productAlreadyInCart) {
+    cart.items.push(product);
+  }// end of if
+  buildCart();
+}// end of function
