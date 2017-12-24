07aab0a1ca189d120754515174fdef7375e97229
Kauress <kauress8@gmail.com>
2017-12-24 22:44:26 +0530

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -129,3 +129,18 @@ function grid() {
       displayItems[i].style.width = "";
     }// end of for
 }// end of function
+
+// onchange function sortByChoice
+document.getElementById("sortByChoice").onchange = function() {
+    var selectElement = document.getElementById("sortByChoice");
+    var selectedOption = selectElement.options[selectElement.selectedIndex].value;
+    if(selectedOption === "LowToHigh") {
+      products.sort(ascendingOrder);
+    } else if(selectedOption === "HighToLow") {
+      products.sort(descendingOrder);
+    } else {
+      products.sort(alphabeticalOrder);
+    }
+    buildDOM();
+}; // end of onchange function
+
