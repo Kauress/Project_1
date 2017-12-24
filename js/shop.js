b3d5daf7ac132b2d26a22326fa0b23530206282e
Kauress <kauress8@gmail.com>
2017-12-24 22:56:47 +0530

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -471,3 +471,11 @@ function shipping() {
     }
     grandTotal();
 }//end of shipping
+
+// function showShipping
+function showShipping() {
+    if(cart.shipping !== 0) {
+        document.getElementById("selectBox").value = cart.shippingLocation;
+    }// end of if
+}// end of function
+
