7cc59c083ea394d05b82a8d219e12e97ba44f090
Kauress <kauress8@gmail.com>
2017-12-24 22:50:21 +0530

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -350,3 +350,17 @@ function getProductInfo(index) {
         table.appendChild(totalRow);
         grandTotal();
  } // end of function
+
+// decrease quantity
+function decreaseQuantity(index) {
+    for(var i = 0; i < cart.items.length; i++) {
+      if(cart.items[i].Id === index) {
+        cart.items[i].Quantity--;
+        cart.items[i].Amount = cart.items[i].Quantity * cart.items[i].Price;
+        if(cart.items[i].Quantity <= 0) {
+          removeItem(index);
+        }// end of if
+      }// end of if
+    }// end of for
+    productTotal();
+}// end of function
